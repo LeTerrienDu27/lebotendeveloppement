895ba67996d9fc8b52253db0572c416e3660df60
LeTerrienDu27 <42359806+LeTerrienDu27@users.noreply.github.com>
2018-08-14 01:04:56 +0200

    Ajouts :
    Ajout d'un préfix : "!"
    Ajout d'une commande : !invite

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 
+var PREFIX = "!";
+
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
@@ -7,4 +9,32 @@ bot.on("ready", function() {
     console.log("Lebot a bien été connecté");
 });
 
-bot.login("NDc4NjU1MDQyMTc4NjQ2MDE3.DlORew.By5ZWkuS1N1ls6rbbIeUWO9ID_U");
+bot.on("message", async function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if(!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split(" ")
+
+    switch(args[0].toLowerCase()) {
+        case "invite":
+        message.channel.send("Test");
+        message.channel.send("", {
+            embed: {
+                color: 0xFF0000,
+                author: message.author.name,
+                title: '',
+                fields: [{
+                    name: "Lien d'invitation discord",
+                    value: "https://discord.gg/MqKvhvB",
+                    inline: false
+                }],
+                footer: {
+                    footer: "Partager ce lien à tous vos amis !",
+                },
+            }
+        });
+        break;
+    }
+});
+ bot.login(process.env.TOKEN);
